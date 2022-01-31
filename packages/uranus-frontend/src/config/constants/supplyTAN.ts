3b7d54433a20c77361e4eae05efa3dcd5f9ceed4
Happy new day! <74836776+neatDevNetizon@users.noreply.github.com>
2022-01-31 17:42:57 +0300

    Update supplyTAN.ts

--- packages/uranus-frontend/src/config/constants/supplyTAN.ts
+++ packages/uranus-frontend/src/config/constants/supplyTAN.ts
@@ -2,18 +2,18 @@ import BigNumber from 'bignumber.js'
 import { getTanContract } from 'utils/contractHelpers'
 import { BLOCKS_PER_YEAR, BLOCKS_PER_MONTH, COE_SUPPLY_VARING } from 'config'
 
-export const supplyTAN = async (): Promise<number> => {
-  const tanContract = getTanContract()
-  const supply = await tanContract.SupplyPerEpoch()
-  const supplyBigNumber = new BigNumber(supply.toString())
-  const supplyDecimalRemoved = supplyBigNumber.div(1000000000000000000)
+// export const supplyTAN = async (): Promise<number> => {
+//   const tanContract = getTanContract()
+//   const supply = await tanContract.SupplyPerEpoch()
+//   const supplyBigNumber = new BigNumber(supply.toString())
+//   const supplyDecimalRemoved = supplyBigNumber.div(1000000000000000000)
 
-  const tanPerMonth = supplyDecimalRemoved.toNumber()
-  const tanPerBlock = tanPerMonth / BLOCKS_PER_MONTH
+//   const tanPerMonth = supplyDecimalRemoved.toNumber()
+//   const tanPerBlock = tanPerMonth / BLOCKS_PER_MONTH
 
-  return tanPerBlock
-}
-
-// export const supplyTAN = (): number => {
-//   return Number(process.env.REACT_APP_TAN_PER_BLOCK)
+//   return tanPerBlock
 // }
+
+export const supplyTAN = (): number => {
+  return Number(process.env.REACT_APP_TAN_PER_BLOCK)
+}
